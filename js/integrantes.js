f5e6f2dd387d9f5ca8d5e98c1b28b0ac552d6651
Angel M. Gonzalez <angegonzalez@unal.edu.co>
2022-08-19 14:26:44 -0500

    Adding some changes

--- js/integrantes.js
+++ js/integrantes.js
@@ -20,13 +20,60 @@
 //     // profesores.style.display = "initial"
 // }
 
-const boton = document.getElementById("btn-profesores")
-boton.focus()
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-});
-const popover = new bootstrap.Popover('.popover-dismiss', {
-    trigger: 'focus'
-})
+const toggleActive = (e) => {
+  console.log(e.target.id);
+  let buttonActiveClass = "plas-button active rounded-4 d-block w-100 my-1 p-2";
+  let buttonClass = "plas-button rounded-4 d-block w-100 my-1 p-2"
+  if (e.target.id === "btn-profesores") {
+    document.getElementById(e.target.id).className = buttonActiveClass;
+    document.getElementById("btn-doctorado").className = buttonClass;
+    document.getElementById("btn-maestria").className = buttonClass;
 
+    let profesores = document.getElementById("profesores");
+    let doctorado = document.getElementById("doctorado");
+    let maestria = document.getElementById("maestria");
+    if (profesores.style.display == "none") {
+        profesores.style.display = "block";
+        doctorado.style.display = "none";
+        maestria.style.display = "none";
+    }
+
+
+  } else if (e.target.id === "btn-doctorado") {
+    document.getElementById(e.target.id).className = buttonActiveClass;
+    document.getElementById("btn-profesores").className = buttonClass;
+    document.getElementById("btn-maestria").className = buttonClass;
+
+    let profesores = document.getElementById("profesores");
+    let doctorado = document.getElementById("doctorado");
+    let maestria = document.getElementById("maestria");
+    if (doctorado.style.display == "none") {
+        doctorado.style.display = "block";
+        profesores.style.display = "none";
+        maestria.style.display = "none";
+    }
+
+
+
+  } else if (e.target.id === "btn-maestria") {
+    document.getElementById(e.target.id).className = buttonActiveClass;
+    document.getElementById("btn-profesores").className = buttonClass;
+    document.getElementById("btn-doctorado").className = buttonClass;
+
+    let profesores = document.getElementById("profesores");
+    let doctorado = document.getElementById("doctorado");
+    let maestria = document.getElementById("maestria");
+    if (maestria.style.display == "none") {
+        maestria.style.display = "block";
+        doctorado.style.display = "none";
+        profesores.style.display = "none";
+    }
+
+  }
+};
+let btn_profesores = document.getElementById("btn-profesores");
+let btn_doctorado = document.getElementById("btn-doctorado");
+let btn_maestria = document.getElementById("btn-maestria");
+btn_profesores.addEventListener("click", toggleActive, false);
+btn_doctorado.addEventListener("click", toggleActive, false);
+btn_maestria.addEventListener("click", toggleActive, false);
---
+++ js/popovers.js
@@ -0,0 +1,9 @@
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+});
+const popover = new bootstrap.Popover('.popover-dismiss', {
+    trigger: 'focus'
+})
+
