b1b0f9062b16afb9c2154e1419a77d3980f023a6
Heldeg <edhernandezfo@unal.edu.co>
2022-12-14 19:36:26 -0500

    update dates with google sheet

--- js/mlds_landing_page/landing_page.js
+++ js/mlds_landing_page/landing_page.js
@@ -1,4 +1,5 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxDJBq_op0k84MZsO6PSZYFGVj3KlqoWrUC0GawJLwG8VhOGbzEvS6QzleONSoCP_jV/exec';
+const formScriptURL = 'https://script.google.com/macros/s/AKfycbxDJBq_op0k84MZsO6PSZYFGVj3KlqoWrUC0GawJLwG8VhOGbzEvS6QzleONSoCP_jV/exec';
+const datesScriptURL = 'https://script.google.com/macros/s/AKfycbxYeUL_ANUZrxqPQtT5ZhtoPQAT5OUgkJ6E6xPr_dgMyO1ovWr9I8Co6BammKm_cX4KJA/exec'
 const form = document.forms['contactForm'];
 const emailInputElement = document.getElementById('emailForm');
 const selectInputElement = document.getElementById('interestForm');
@@ -6,28 +7,28 @@ const selectInputElement = document.getElementById('interestForm');
 
 const isInvalidClassName = 'is-invalid'
 
-function validateEmail(emailText){
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailText)   
+function validateEmail(emailText) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailText)
 }
 
-function validateInterestSelect(selectElement){
+function validateInterestSelect(selectElement) {
     return selectElement.value !== "" || selectElement.value !== null;
 }
 
-function checkForm(){
+function checkForm() {
 
     let isInputsValid = true;
 
-    if(!validateEmail(emailInputElement.value)){
+    if (!validateEmail(emailInputElement.value)) {
         emailInputElement.classList.add(isInvalidClassName);
         isInputsValid = false;
     }
 
-    if(!validateInterestSelect(selectInputElement)){
+    if (!validateInterestSelect(selectInputElement)) {
         selectInputElement.classList.add(isInvalidClassName);
         isInputsValid = false;
     }
-    if(!isInputsValid){
+    if (!isInputsValid) {
         return isInputsValid
     }
     emailInputElement.classList.remove(isInvalidClassName);
@@ -35,33 +36,74 @@ function checkForm(){
     return isInputsValid;
 }
 
-function showSuccesFormAlert(){
+function showSuccesFormAlert() {
     const succesAlertElement = document.getElementById('succesAlertForm');
     succesAlertElement.hidden = false;
-    setTimeout(() => { succesAlertElement.hidden = true;}, 5000)
+    setTimeout(() => { succesAlertElement.hidden = true; }, 5000)
 }
 
-function showErrorFormAlert(){
+function showErrorFormAlert() {
     const succesErrorElement = document.getElementById('errorAlertForm');
     succesErrorElement.hidden = false;
-    setTimeout(() => {succesErrorElement.hidden = true;}, 5000)
+    setTimeout(() => { succesErrorElement.hidden = true; }, 5000)
 }
 
+function getDates() {
+    fetch(datesScriptURL, {
+        method: 'GET', headers: {
+            'Accept': 'application/json',
+        },
+    })
+        .then(response => {return response.json()}).then(dates => updateDate(dates.values))
+        .catch(response => console.log(response))
+}
+
+function updateDate(dateInfo) {
+    dateInfo.forEach(modInfo => {
+        const modNumber = modInfo['module']
+        const startDayEl = document.getElementById(`mod-${modNumber}-startDay`)
+        const startMonthEl = document.getElementById(`mod-${modNumber}-startMonth`)
+        const endDayEl = document.getElementById(`mod-${modNumber}-endDay`)
+        const endMonthEl = document.getElementById(`mod-${modNumber}-endMonth`)
+
+        const startDate = modInfo['start_date'].split('/')
+        const endDate = modInfo['end_date'].split('/')
+
+        startDayEl.innerHTML = startDate[0]
+        startMonthEl.innerHTML = getMonthName(startDate[1])
+        endDayEl.innerHTML = endDate[0]
+        endMonthEl.innerHTML = getMonthName(endDate[1])
+
+    })
+}
+
+
+function getMonthName(monthNumber) {
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+
+    return date.toLocaleString('es', { month: 'long' });
+}
+
+
+
 emailInputElement.addEventListener('change', e => {
-    if(!validateEmail(emailInputElement.value)){
+    if (!validateEmail(emailInputElement.value)) {
         emailInputElement.classList.add(isInvalidClassName);
-    }else {
+    } else {
         emailInputElement.classList.remove(isInvalidClassName);
     }
 });
 
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  if(checkForm()){
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(showSuccesFormAlert())
-    .catch(showErrorFormAlert())
-  }
-  
+    e.preventDefault()
+    if (checkForm()) {
+        fetch(formScriptURL, { method: 'POST', body: new FormData(form) })
+            .then(showSuccesFormAlert())
+            .catch(showErrorFormAlert())
+    }
+
 });
+
+getDates()
