f7b7bfee2a68e1be9c801dad8ce13a6b3ef265f6
Heldeg <edhernandezfo@unal.edu.co>
2023-07-23 18:52:56 -0500

    show and hide content

--- js/mlds_landing_page/survey.js
+++ js/mlds_landing_page/survey.js
@@ -1,4 +1,11 @@
 const knowdledgeListURL = 'https://raw.githubusercontent.com/Heldeg/test-knowdledge-list/main/knowledge-list.json';
+const prevBtn = document.getElementById("surveyPrevBtb");
+const nextBtn = document.getElementById("surveyNextBtb");
+const sendBtn = document.getElementById("surveySendBtb");
+
+let numPages = 0;
+let currentPage = 0;
+let knowdledgeList = {}
 
 async function getknowledgeList() {
     const response = await fetch(knowdledgeListURL);
@@ -35,7 +42,7 @@ function createModList(modNumber, moduleInfo, knowledgeExtraClasses = "") {
     for (let item of knowdledgeItems) {
         checkboxItems += item + "\n"
     }
-    const moduleList = `<div class="survey-knowledge" id="mod-${modNumber}">
+    const moduleList = `<div class="survey-knowledge">
     ${knowledgeExtraClasses ? "" : `<h2>Módulo ${modNumber}</h2>`}
     <div class="survey-knowledge-list ${knowledgeExtraClasses}">
         ${checkboxItems}
@@ -45,20 +52,24 @@ function createModList(modNumber, moduleInfo, knowledgeExtraClasses = "") {
 }
 
 function loadMod0(module0Info) {
-    const mod0div = document.getElementById("basicRequirements");
+    const mod0div = document.getElementById("mod-0-list");
     mod0div.innerHTML = createModList(0, module0Info, knowledgeExtraClasses = "d-flex justify-content-start")
 }
 
 async function loadLists() {
     try {
-        const knowdledgeList = await getknowledgeList();
+        knowdledgeList = await getknowledgeList();
         const modsRow = document.getElementById("modRequirements");
+        const numModules = Object.keys(knowdledgeList).length;
+        //Mod 0 is in first page, therefore subtract one
+        numPages = parseInt((numModules - 1) / 3);
+        currentPage = 1;
 
         loadMod0(knowdledgeList["0"]);
 
         let htmlModContent = '';
-        for (let i = 1; i < Object.keys(knowdledgeList).length; i++) {
-            htmlModContent += `<div class="col-md-4" ${i>3 ? "hidden": ""}>${createModList(i, knowdledgeList[i])}</div>`;
+        for (let i = 1; i < numModules; i++) {
+            htmlModContent += `<div class="col-md-4 animation" ${i > 3 ? "hidden" : ""} id="mod-${i}-list">${createModList(i, knowdledgeList[i])}</div>`;
         }
 
         modsRow.innerHTML = htmlModContent;
@@ -67,5 +78,76 @@ async function loadLists() {
         console.log(error);
     }
 }
+
+function updateSurvey() {
+    toggleBtns();
+    toggleKnowledgeList();
+}
+
+function toggleBtns() {
+    if (currentPage < numPages) {
+        nextBtn.removeAttribute("hidden");
+    } else {
+        nextBtn.setAttribute('hidden', 'true');
+    }
+
+    if (currentPage > 1) {
+        prevBtn.removeAttribute("hidden");
+    } else {
+        prevBtn.setAttribute('hidden', 'true');
+    }
+
+    if (currentPage === numPages) {
+        sendBtn.removeAttribute("hidden");
+    } else {
+        sendBtn.setAttribute('hidden', 'true');
+    }
+}
+
+function toggleKnowledgeList() {
+    const minMod = currentPage * 3 - 2
+    const maxMod = currentPage * 3
+    const numModules = Object.keys(knowdledgeList).length;
+    const mod0div = document.getElementById("mod-0-list");
+
+    if (currentPage === 1) {
+        setTimeout(() => {mod0div.removeAttribute("hidden") }, 200);
+        setTimeout(() => {
+            mod0div.classList.remove("hide");
+        }, 100);
+    } else {
+        mod0div.classList.add("hide");
+        setTimeout(() => {
+            mod0div.setAttribute('hidden', 'true');
+        }, 200);
+
+    }
+
+
+
+    for (let i = 1; i < numModules; i++) {
+        let modeCol = document.getElementById(`mod-${i}-list`);
+        if (i >= minMod && i <= maxMod) {
+            setTimeout(() => {modeCol.removeAttribute("hidden") }, 200);
+            setTimeout(() => {
+                modeCol.classList.remove("hide");
+            }, 100);
+        } else {
+            modeCol.classList.add("hide");
+            setTimeout(() => { modeCol.setAttribute('hidden', 'true'); }, 200);
+        }
+    }
+
+}
+
+prevBtn.addEventListener("click", e => {
+    currentPage -= 1;
+    updateSurvey();
+});
+
+nextBtn.addEventListener("click", e => {
+    currentPage += 1;
+    updateSurvey();
+});
 
 loadLists();
